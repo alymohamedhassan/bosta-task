c03b95bd4f2d84f81109be0ebba7193e76afbf2a
Aly <alyhassan10@hotmail.com>
2023-12-30 22:45:27 +0200

    feat: add borrowing process module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { BooksModule } from './books/books.module';
 import { BorrowersModule } from './borrowers/borrowers.module';
 import { CommonModule } from './common/common.module';
 import { AuthorsModule } from './authors/authors.module';
+import { BorrowingProcessesModule } from './borrowing-processes/borrowing-processes.module';
 
 @Module({
-  imports: [BooksModule, BorrowersModule, CommonModule, AuthorsModule],
+  imports: [BooksModule, BorrowersModule, CommonModule, AuthorsModule, BorrowingProcessesModule],
   controllers: [AppController],
   providers: [AppService],
 })
