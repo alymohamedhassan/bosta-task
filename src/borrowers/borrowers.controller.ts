7674c873dc29434a2e1c680817cacb219d26fd22
Aly <alyhassan10@hotmail.com>
2023-12-30 15:49:13 +0200

    implement findone in controller

--- src/borrowers/borrowers.controller.ts
+++ src/borrowers/borrowers.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, NotFoundException, HttpCode } from '@nestjs/common';
 import { BorrowersService } from './borrowers.service';
 import { CreateBorrowerDto, UpdateBorrowerDto } from './dto/borrower.dto';
 import { ResponseTransform } from 'src/common/interceptors/response.interceptor';
+import { throws } from 'assert';
+import { NotEquals } from 'class-validator';
 
 @Controller('borrowers')
 export class BorrowersController {
@@ -29,17 +31,26 @@ export class BorrowersController {
   @Get(':id')
   @UseInterceptors(ResponseTransform)
   async findOne(@Param('id') id: string) {
-    return this.borrowersService.findOne(+id);
+    const exists = await this.borrowersService.existsById(+id);
+    if (!exists)
+      throw new NotFoundException("Borrower not found")
+
+    const borrower = await this.borrowersService.findOne(+id);
+    return {
+      borrower,
+    }
   }
 
   @Patch(':id')
   @UseInterceptors(ResponseTransform)
-  async update(@Param('id') id: string, @Body() borrower: UpdateBorrowerDto) {
-    return this.borrowersService.update(+id, borrower);
+  @HttpCode(204)
+  async update(@Param('id') id: string, @Body() body: UpdateBorrowerDto) {
+    return this.borrowersService.update(+id, body);
   }
 
   @Delete(':id')
   @UseInterceptors(ResponseTransform)
+  @HttpCode(204)
   async delete(@Param('id') id: string) {
     return this.borrowersService.remove(+id);
   }
