a7b7fb75f821840c743fd17080d45743ddc023b9
Aly <alyhassan10@hotmail.com>
2023-12-31 00:23:11 +0200

    feat: rate limiting on book checkout

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,15 +2,28 @@ import { Module } from '@nestjs/common';
 import { AuthGuard } from './guards/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RateLimitGuard } from './guards/rate-limiting.guard';
+import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
+  imports: [
+    NestRedisModule.forRoot({
+      config: {
+        url: process.env.REDIS_URL,
+      },
+    }),
+  ],
   controllers: [],
   providers: [
+    PrismaService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
-    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: RateLimitGuard,
+    },
   ],
 })
 export class CommonModule {}
---
+++ src/common/guards/rate-limiting.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { routeMatching } from '../utils/route-matching';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Redis } from 'ioredis';
+
+const includedRoutes = [
+  {
+    path: 'borrowing-process',
+    methods: ['POST']
+  },
+]
+
+@Injectable()
+export class RateLimitGuard implements CanActivate {
+  constructor(@InjectRedis() private redis: Redis) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (!routeMatching(request, includedRoutes)) return true;
+
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(request: any) {
+    const now = new Date()
+    const time = String(now.toISOString()).split(":");
+    const timeTillMins = time.filter((value) => value != time[time.length - 1]).join(":");
+
+    const old = await this.redis.get(String(timeTillMins));
+    await this.redis.set(String(timeTillMins), String((parseInt(old) || 0) + 1));
+
+    const newBucket = await this.redis.get(String(timeTillMins));
+    console.log("New:", newBucket);
+
+    if (parseInt(newBucket) >= parseInt(process.env.RATE_LIMIT_CHECKOUT))
+      throw new HttpException("Too many requests", HttpStatus.TOO_MANY_REQUESTS)
+
+    return true;
+  }
+}
