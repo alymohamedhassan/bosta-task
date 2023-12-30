d81fa4c3a687de24e3c5f7737b2cc95c2bde62cd
Aly <alyhassan10@hotmail.com>
2023-12-30 14:54:42 +0200

    add authors module to book module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthorsService } from 'src/authors/authors.service';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService, PrismaService],
+  providers: [BooksService, AuthorsService, PrismaService],
 })
 export class BooksModule {}
