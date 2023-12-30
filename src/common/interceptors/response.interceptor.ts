806f6fd800341b0c65f45f1073932d30af2624c6
Aly <alyhassan10@hotmail.com>
2023-12-30 11:22:42 +0200

    add response interceptors

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class ResponseTransform implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          content: data,
+        };
+      }),
+    );
+  }
+}
