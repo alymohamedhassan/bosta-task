b3cb4b02540cc80f22cd3f8031d5b64fad62cd89
Aly <alyhassan10@hotmail.com>
2023-12-30 11:23:59 +0200

    add book and borrowers modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { BorrowersModule } from './borrowers/borrowers.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, BorrowersModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
