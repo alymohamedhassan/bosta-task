63b7ad46c41cbcb4578b092a503253b412b11135
Aly <alyhassan10@hotmail.com>
2023-12-30 15:42:13 +0200

    add response interceptor

--- src/borrowers/borrowers.controller.ts
+++ src/borrowers/borrowers.controller.ts
@@ -1,33 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
 import { BorrowersService } from './borrowers.service';
 import { CreateBorrowerDto, UpdateBorrowerDto } from './dto/borrower.dto';
+import { ResponseTransform } from 'src/common/interceptors/response.interceptor';
 
 @Controller('borrowers')
 export class BorrowersController {
   constructor(private readonly borrowersService: BorrowersService) {}
 
   @Post()
-  create(@Body() createBorrowerDto: CreateBorrowerDto) {
-    return this.borrowersService.create(createBorrowerDto);
+  @UseInterceptors(ResponseTransform)
+  async create(@Body() body: CreateBorrowerDto) {
+    const borrower = await this.borrowersService.create(body);
+
+    return {
+      borrower,
+    }
   }
 
   @Get()
-  findAll() {
-    return this.borrowersService.findAll();
+  @UseInterceptors(ResponseTransform)
+  async findAll() {
+    const borrowers = await this.borrowersService.findAll();
+    return {
+      borrowers,
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @UseInterceptors(ResponseTransform)
+  async findOne(@Param('id') id: string) {
     return this.borrowersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBorrowerDto: UpdateBorrowerDto) {
-    return this.borrowersService.update(+id, updateBorrowerDto);
+  @UseInterceptors(ResponseTransform)
+  async update(@Param('id') id: string, @Body() borrower: UpdateBorrowerDto) {
+    return this.borrowersService.update(+id, borrower);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @UseInterceptors(ResponseTransform)
+  async delete(@Param('id') id: string) {
     return this.borrowersService.remove(+id);
   }
 }
