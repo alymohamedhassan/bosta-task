b08deeee76c0b82011eb943f46b472d01f0379a5
Aly <alyhassan10@hotmail.com>
2023-12-30 15:40:39 +0200

    add email and password to dto

--- src/borrowers/dto/borrower.dto.ts
+++ src/borrowers/dto/borrower.dto.ts
@@ -2,6 +2,8 @@ import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateBorrowerDto {
   name: string;
+  email: string;
+  password: string;
 }
 
 export class UpdateBorrowerDto extends PartialType(CreateBorrowerDto) {}
