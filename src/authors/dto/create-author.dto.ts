e30c0c6b641813acf6af0aefe77a33178d7e3356
Aly <alyhassan10@hotmail.com>
2023-12-30 14:02:48 +0200

    add author validation

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateAuthorDto {
+  @IsNotEmpty({ message: 'Author name cannot be empty' })
+  @IsString({ message: 'Author name has to be a string' })
   name: string;
 }
