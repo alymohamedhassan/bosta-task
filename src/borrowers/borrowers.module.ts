4f71be81d84a959ce7bd1ca5f19f7efa49b4d514
Aly <alyhassan10@hotmail.com>
2023-12-31 17:44:17 +0200

    refactor: add missing provider

--- src/borrowers/borrowers.module.ts
+++ src/borrowers/borrowers.module.ts
@@ -3,9 +3,10 @@ import { BorrowersService } from './borrowers.service';
 import { BorrowersController } from './borrowers.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BorrowingProcessesService } from 'src/borrowing-processes/borrowing-processes.service';
+import { BooksService } from 'src/books/books.service';
 
 @Module({
   controllers: [BorrowersController],
-  providers: [BorrowersService, PrismaService, BorrowingProcessesService],
+  providers: [BorrowersService, BooksService, PrismaService, BorrowingProcessesService],
 })
 export class BorrowersModule {}
