ff24f46aad4c46877e3a2470aa026fb6bbf7f2ae
Aly <alyhassan10@hotmail.com>
2023-12-31 13:05:17 +0200

    feat: add validation on borrower DRO

--- src/borrowers/dto/borrower.dto.ts
+++ src/borrowers/dto/borrower.dto.ts
@@ -1,13 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateBorrowerDto {
   @ApiProperty()
+  @IsString({message: 'Name has to be a string'})
   name: string;
 
   @ApiProperty()
+  @IsString({message: 'Email has to be a string'})
+  @IsEmail({message: 'Email not valid'})
   email: string;
 
   @ApiProperty()
+  @IsString({message: 'Password has to be a string'})
   password: string;
 }
 
